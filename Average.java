13460cf59468a4bca1cac57e7bde947043605c27
Shaik Rufiya <150911765+Rufiya09@users.noreply.github.com>
2023-12-18 16:38:45 +0530

    8) Write a Java method to compute the average of three numbers.

---
+++ Average.java
@@ -0,0 +1,16 @@
+//Write a Java method to compute the average of three numbers. 
+
+class Average
+{
+	public static void main(String args[])
+	{
+	int num1 = 25;
+	int num2 = 56;
+	int num3 = 85;
+	
+	int avg = (num1+num2+num3)/3 ;
+	
+	System.out.println("the average of three numbers are : " + avg );
+	
+	}
+}
