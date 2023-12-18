9a13d01d0d4d8bbee5982c8d93650b15447a2594
Shaik Rufiya <150911765+Rufiya09@users.noreply.github.com>
2023-12-18 16:43:50 +0530

    6) Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers

---
+++ Operators.java
@@ -0,0 +1,27 @@
+//Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers
+
+class Operators
+{
+	public static void main(String args[])
+	{
+	  int num1 = 85;
+	  int num2 = 52;
+	  
+	  int sum = num1+num2;
+	  int sub = num1-num2;
+	  int mul = num1*num2;
+	  int avg = sum/2 ;
+	  int rem = sum%2;
+	  
+	System.out.println("the sum is");
+	System.out.println(sum);
+	System.out.println("the sub is");
+	System.out.println(sub);
+	System.out.println("the mul is");
+    System.out.println(mul);
+	System.out.println("the avg is");
+	System.out.println(avg);
+	System.out.println("the rem is");
+	System.out.println(rem);
+	}
+}
